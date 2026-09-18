695f45561ab53eaea99ec9e619e03e7725026798
Kaushik <kaushik.lf07@gmail.com>
2026-09-18 23:42:03 +0530

    Updated redis env variables

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,6 +1,13 @@
 import { createClient } from "redis";
 
-const redisClient = createClient();
+const redisClient = createClient({
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+});
 
 redisClient.on("error", (err) => {
   console.log("Redis Client Error", err);
