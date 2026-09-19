b6485235f85a5647f65a80c94090c89d97894383
Kaushik <kaushik.lf07@gmail.com>
2026-09-19 23:47:59 +0530

    Add root route

--- server/app.js
+++ server/app.js
@@ -22,6 +22,10 @@ app.use(
   })
 );
 
+app.get("/", (req, res)=>{
+  res.json({message: "Hello from storageApp!"})
+})
+
 app.use("/directory", checkAuth, directoryRoutes);
 app.use("/file", checkAuth, fileRoutes);
 app.use("/", userRoutes);
